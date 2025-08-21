a16f52c93e0b4d5f81b2346a0dff280b7eec5098
Andrew Hall <andrewmartinhall2@gmail.com>
2025-08-20 18:01:24 -0600

    Added result documentation for fund, all_funds, and benchmark routes.

--- app/models/fund.py
+++ app/models/fund.py
@@ -30,6 +30,25 @@ class FundSummaryResponse(BaseModel):
     tracking_error: float
     information_ratio: float
 
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "fund": "grad",
+                "start": "2024-08-20",
+                "end": "2025-08-20",
+                "value": 1739522.36818768,
+                "total_return": 20.46126675427733,
+                "sharpe_ratio": 1.1305155062718004,
+                "volatility": 18.09905891671865,
+                "dividends": 16522.01,
+                "dividend_yield": 0.9498015261058954,
+                "alpha": 0.049038466899782446,
+                "beta": 0.942991340240139,
+                "tracking_error": 3.62578673540609,
+                "information_ratio": 0.013524917618821316
+            }
+        }
+
 class FundRecord(BaseModel):
     date: date
     value: float
@@ -44,4 +63,33 @@ class FundTimeSeriesResponse(BaseModel):
     start: date
     end: date
     records: list[FundRecord]
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "fund": "grad",
+                "start": "2024-08-20",
+                "end": "2025-08-20",
+                "records": [
+                    {
+                    "date": "2024-08-20",
+                    "value": 1440189.47043864,
+                    "return_": -0.26742265271267684,
+                    "cummulative_return": -0.26742265271267884,
+                    "dividends": 0,
+                    "benchmark_return": -0.25122084490348,
+                    "benchmark_cummulative_return": -0.25122084490347696
+                    },
+                    {
+                    "date": "2024-08-21",
+                    "value": 1451636.34043864,
+                    "return_": 0.7948169483917699,
+                    "cummulative_return": 0.525268775111476,
+                    "dividends": 0,
+                    "benchmark_return": 0.37146868608184797,
+                    "benchmark_cummulative_return": 0.11931463440664203
+                    }
+                ]
+            }
+        }
 
