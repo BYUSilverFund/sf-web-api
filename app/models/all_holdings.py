19ab8b69c8c036ef3df502b39b7a7d3b75e6e647
andrewhall1124 <andrewmartinhall2@gmail.com>
2025-09-03 21:18:31 -0600

    Changed all_holdings to include annualized flag.

--- app/models/all_holdings.py
+++ app/models/all_holdings.py
@@ -6,10 +6,11 @@ class AllHoldingsRequest(BaseModel):
     fund: str
     start: date
     end: date
+    annualized: bool
 
     class Config:
         json_schema_extra = {
-            "example": {"fund": "grad", "start": "2024-08-20", "end": "2025-08-20"}
+            "example": {"fund": "grad", "start": "2024-08-20", "end": "2025-08-20", "annualized": False}
         }
 
 
