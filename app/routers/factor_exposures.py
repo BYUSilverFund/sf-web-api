0a5bf50b09d5009386d5d006c2db31467dd29fd7
Ben Haggard <21bhaggard@gmail.com>
2025-12-18 16:25:42 -0700

    factor exposures implemented

--- app/routers/factor_exposures.py
+++ app/routers/factor_exposures.py
@@ -1,25 +1,47 @@
-import io
-
 from fastapi import APIRouter
-from fastapi.responses import StreamingResponse
 
-from app.services.factor_exposures import get_factor_exposures
+from app.services.factor_exposures import (
+    all_fund_weighted_exposures,
+    fund_weighted_exposures,
+)
 
 
 router = APIRouter()
 
+# # saving for future implementation
+# @router.get(
+#     "/latest",
+#     summary="Get Factor Exposure Matrix",
+#     description="Returns entire factor exposures matrix",
+#     response_description="Latest factor exposure matrix",
+#     tags=["Factor Exposures"],
+# )
+# def factor_exposures() -> StreamingResponse:
+#     factor_exposures = get_factor_exposures()
+#     csv_string = factor_exposures.write_csv()
+#     csv_io = io.StringIO(csv_string)
+#     headers = {"Content-Disposition": "attachment; filename=factor_exposures.csv"}
+#     media_type = "text/csv"
+#     return StreamingResponse(content=csv_io, headers=headers, media_type=media_type)
 
-@router.post(
-    "/latest",
-    summary="Get Latest Factor Exposures",
-    description="Returns latest factor exposures",
-    response_description="Latest factor exposures",
-    tags=["Covariance Matrix"],
+
+@router.get(
+    "/fund/{fund}",
+    summary="Get Fund Factor Exposures",
+    description="Returns factor exposures for a specific fund",
+    response_description="Factor exposures for the specified fund",
+    tags=["Factor Exposures"],
 )
-def factor_exposures() -> StreamingResponse:
-    factor_exposures = get_factor_exposures()
-    csv_string = factor_exposures.write_csv()
-    csv_io = io.StringIO(csv_string)
-    headers = {"Content-Disposition": "attachment; filename=factor_exposures.csv"}
-    media_type = "text/csv"
-    return StreamingResponse(content=csv_io, headers=headers, media_type=media_type)
+def fund_exposures(fund: str) -> dict:
+    return fund_weighted_exposures(fund)
+
+
+@router.get(
+    "/all_funds",
+    summary="Get All Funds Factor Exposures",
+    description="Returns factor exposures for all funds",
+    response_description="Factor exposures for all funds",
+    tags=["Factor Exposures"],
+)
+def all_fund_exposures() -> dict:
+    return all_fund_weighted_exposures()
--- app/services/factor_exposures.py
+++ app/services/factor_exposures.py
@@ -1,10 +1,83 @@
 import polars as pl
+from fastapi import HTTPException
 
 from app import s3
+from app.db import engine
+from app.utils import get_account_id_from_name
 
 
 def get_factor_exposures() -> pl.DataFrame:
     return s3.scan_parquet(
         bucket_name="barra-factor-exposures",
-        file_key="latest.parquet",
+        file_key="latest_exposures.parquet",
     ).collect()
+
+
+# you need to convert fund to client account id
+#  holding returns already has weights calculated
+def fund_holding_weights(client_account_id: str) -> pl.DataFrame:
+    weights = pl.read_database(
+        query=f"""
+            SELECT
+                client_account_id,
+                ticker,
+                weight
+            FROM holding_returns
+            WHERE date = (select max(date) from holding_returns)
+                AND client_account_id = '{client_account_id}'
+            """,
+        connection=engine,
+    )
+    return pl.DataFrame(weights)
+
+
+def all_fund_holding_weights() -> pl.DataFrame:
+    quant_paper_id = get_account_id_from_name("quant_paper")
+    weights = pl.read_database(
+        # FIXME: I dont think the weights are correct. I am getting .97 for all funds combined
+        query=f"""
+            SELECT 
+                ticker,
+                SUM(value) / SUM(SUM(value)) OVER () AS weight
+            FROM holding_returns
+            WHERE date = (SELECT MAX(date) FROM holding_returns)
+                AND client_account_id != '{quant_paper_id}'
+            GROUP BY ticker;
+            """,
+        connection=engine,
+    )
+    return pl.DataFrame(weights)
+
+
+def compute_exposure_weights(
+    exposures: pl.DataFrame, weights: pl.DataFrame
+) -> pl.DataFrame:
+    combo = exposures.join(weights, on="ticker", how="inner").drop(
+        [
+            col
+            for col in ["client_account_id", "date", "ticker"]
+            if col in exposures.columns
+        ]
+    )
+    portfolio_exposure = combo.fill_null(0).select(
+        (pl.col("^USSLOWL_.*$") * pl.col("weight")).sum()
+    )
+    return portfolio_exposure
+
+
+def fund_weighted_exposures(fund: str):
+    exposures = get_factor_exposures()
+    try:
+        client_account_id = get_account_id_from_name(fund)
+    except ValueError:
+        raise HTTPException(status_code=404, detail=f"Fund '{fund}' not found")
+    portfolio_exposure = compute_exposure_weights(
+        exposures, fund_holding_weights(client_account_id)
+    )
+    return portfolio_exposure.to_dicts()[0]
+
+
+def all_fund_weighted_exposures():
+    exposures = get_factor_exposures()
+    weights = all_fund_holding_weights()
+    return compute_exposure_weights(exposures, weights).to_dicts()[0]
