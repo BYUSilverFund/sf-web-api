555fc78bb50ea33e555ac4e6a171bf33bc7eada9
Jake Gunnell <gunnjakeim@gmail.com>
2025-12-20 11:22:11 -0800

    Data download (#26)
    * Add CSV download route for all funds time series performance
    
    * Data-Download TimeSeries for all Funds
    
    * v
    
    * changed file and function names
    
    * remove 2 changes
    
    * Data Download project Added CSV export endpoints
    
    * Data Download project Added CSV export endpoints
    
    * Fixed path naming
    
    * merge conflict fixes
    
    * line ending normalization
    
    ---------
    
    Co-authored-by: Ben Haggard <21bhaggard@gmail.com>
    Co-authored-by: Andrew Hall <95375458+andrewhall1124@users.noreply.github.com>

--- app/services/portfolio.py
+++ app/services/portfolio.py
@@ -219,3 +219,11 @@ def get_portfolio_time_series(request: PortfolioRequest) -> dict[str, any]:
     }
 
     return result
+
+
+def get_portfolio_time_series_csv(request: PortfolioRequest) -> bytes:
+    portfolio_ts = get_portfolio_time_series(request)
+    records = portfolio_ts["records"]
+    df = pl.DataFrame(records)
+    csv_bytes = df.write_csv().encode("utf-8")
+    return csv_bytes
