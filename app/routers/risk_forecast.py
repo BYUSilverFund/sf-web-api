8924ef025b4e1d504e53bb7fbf2b68f1f729ca08
benhagg <153319291+benhagg@users.noreply.github.com>
2026-02-17 11:29:48 -0700

    adding holdings for risk forcast (#54) (#57)
    * adding holdings for risk forcast
    
    * logic fix for tracking error & individual holdings
    
    * little fixes for easier frontend
    
    * fixing a typo
    
    Co-authored-by: Gracee Putnam <gracee.putnam@gmail.com>

--- app/routers/risk_forecast.py
+++ app/routers/risk_forecast.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, Depends
 from app.auth import cognito_auth
 from app.services.risk_forecast import (
     all_funds_risk_forecast,
+    fund_holding_risk_forecast,
     fund_risk_forecast,
 )
 
@@ -13,7 +14,7 @@ router = APIRouter()
 @router.get(
     "/all_funds",
     summary="Get All Funds Risk Forecast",
-    description="Returns beta, variance, and volatility forecast for the aggregate of all funds",
+    description="Returns beta, volatility, and tracking error forecast for the aggregate of all funds",
     response_description="Portfolio risk forecast for all funds",
     tags=["Risk Forecast"],
 )
@@ -24,9 +25,22 @@ def all_fund_risk_forecasts(_claims=Depends(cognito_auth)) -> dict:
 @router.get(
     "/{fund}",
     summary="Get Single Fund Risk Forecast",
-    description="Returns beta, variance, and volatility forecast for a single fund",
+    description="Returns beta, volatility, and tracking error for a single fund",
     response_description="Portfolio risk forecast for specified fund",
     tags=["Risk Forecast"],
 )
 def fund_risk_forecasts(fund: str, _claims=Depends(cognito_auth)) -> dict:
     return fund_risk_forecast(fund)
+
+
+@router.get(
+    "/{fund}/holdings/{ticker}",
+    summary="Get Single Fund Holding Risk Forecast",
+    description="Returns risk forecast metrics for a single holding within a fund",
+    response_description="Holding-level risk forecast for specified fund and ticker",
+    tags=["Risk Forecast"],
+)
+def fund_holding_risk_forecasts(
+    fund: str, ticker: str, _claims=Depends(cognito_auth)
+) -> dict:
+    return fund_holding_risk_forecast(fund, ticker)
